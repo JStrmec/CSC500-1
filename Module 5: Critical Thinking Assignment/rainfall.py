2097ee649e6d6decf501cccee16ff6de5caa5bd9
Jocelyn Strmec <strj_vienna@yahoo.com>
2024-02-18 11:57:35 -0800

    assignment 5

--- Module 5: Critical Thinking Assignment/rainfall.py
+++ Module 5: Critical Thinking Assignment/rainfall.py
@@ -2,17 +2,18 @@ def ask_number_of_years():
     return int(input('Enter the number of years to calculate rainfall: '))
 
 def ask_inches_of_rainfall(years):
+    total = 0.0
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     for year in range(years):
         for month in months:
-            total += float(input('Enter the inches rainfall for ' + month + ' ' + str(year+1) + ': '))
+            total += float(input('Enter the inches rainfall for ' + month + ' year ' + str(year+1) + ': '))
     return total
 
-def main()
-    total = 0.0
+def main():
     years = ask_number_of_years()
     total = ask_inches_of_rainfall(years)
     months = years * 12
     print('The total rainfall for', months, 'months is', total, 'inches')
-    print('The average monthly rainfall for', months, 'months is', total/months, 'inches')
+    print(f'The average monthly rainfall fo {months:.3f} months is{(total/months):.3f} inches')
+
 main()
