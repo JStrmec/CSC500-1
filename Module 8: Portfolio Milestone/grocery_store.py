80f4de619d050bdbbce74e482d193381a9a609c8
Jocelyn Strmec <strj_vienna@yahoo.com>
2024-03-09 13:23:28 -0800

    Module 8

--- Module 8: Portfolio Milestone/grocery_store.py
+++ Module 8: Portfolio Milestone/grocery_store.py
@@ -1,33 +1,43 @@
 from shopping import ItemtoPurchase, ShoppingCart
 
-def add_item():
+def add_item() -> ItemtoPurchase:
     item_name = get_item()
+    item_description = input("Enter the item description: ")
     item_price = float(input("Enter the item price: "))
     item_quantity = int(input("Enter the item quantity: "))
-    return ItemtoPurchase(item_name, item_price, item_quantity)
+    return ItemtoPurchase(
+        item_name=item_name,
+        item_description=item_description, 
+        item_price=item_price, 
+        item_quantity=item_quantity)
 
-def get_item():
+def get_item()-> str:
     item_name = input("Enter the item name: ")
     return item_name
 
-def total_cost_(items):
+def get_item_to_remove() -> str:
+    item_name = input("Enter name of item to remove: ")
+    return item_name
+
+def total_cost_(items: list[ItemtoPurchase]) -> float:
     total_cost = 0
     for item in items:
         total_cost += item.item_price * item.item_quantity
     return total_cost
 
-def reciept(items, total_cost):
+def reciept(items: list[ItemtoPurchase], total_cost: float) -> str:
     print("RECIEPT")
     print("TOTAL COST")
     for item in items:
         item.print_item_cost()
     print(f"Total: ${total_cost:.2f}")
 
-def menu_choice(ShopppingCart, choice):
+def menu_choice(ShopppingCart: ShoppingCart, choice: str) -> None:
     if choice == 'a':
         ShopppingCart.add_item(add_item())
     elif choice == 'r':
-        item_name = get_item()
+        print("REMOVE ITEM FROM CART")
+        item_name = get_item_to_remove()
         ShopppingCart.remove_item(item_name)
     elif choice == 'c':
         item_name = get_item()
@@ -37,12 +47,12 @@ def menu_choice(ShopppingCart, choice):
     elif choice == 'o':
         ShopppingCart.print_cart()
     elif choice == 'q':
-        return False
+        return
     else:
         print("Invalid option")
     print_menu(ShopppingCart)
 
-def print_menu(ShopppingCart):
+def print_menu(ShopppingCart) -> None:
     print("MENU")
     print("a - Add item to cart")
     print("r - Remove item from cart")
@@ -54,10 +64,10 @@ def print_menu(ShopppingCart):
     menu_choice(ShopppingCart, choice)
 
 
-def main():
+def main() -> None:
     print("Welcome to the Grocery Store")
     customer_name = input("Enter your name: ")
     current_date = input("Enter the current date: ")
-    ShopppingCart = ShoppingCart(customer_name)
+    ShopppingCart = ShoppingCart(customer_name=customer_name, current_date=current_date)
     print_menu(ShopppingCart)
 main()
--- Module 8: Portfolio Milestone/shopping.py
+++ Module 8: Portfolio Milestone/shopping.py
@@ -1,44 +1,68 @@
 from lorem_text import lorem
+from enum import Enum
 
 class ItemtoPurchase():
-    def __init__(self, item_name = "none", item_price = 0, item_quantity = 0):
+    def __init__(
+            self, 
+            item_name: str = "none",
+            item_description: str = lorem.sentence(), 
+            item_price: int = 0, 
+            item_quantity: int = 0
+        ) -> None:
         self.item_name = item_name
         self.item_price = item_price
         self.item_quantity = item_quantity
-        self.description = lorem.sentence()
+        self.description = item_description
 
-    def print_item_cost(self):
-        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${(self.item_price * self.item_quantity):.2f}")
+    def print_item_cost(self) -> None:
+        print(f"""{self.item_name} {self.item_quantity} @ 
+              ${self.item_price:.2f} = $
+              {(self.item_price * self.item_quantity):.2f}""")
 
 class ShoppingCart():
-    def __init__(self, customer_name = "none", current_date = "January 1, 2020"):
+    def __init__(self, 
+            customer_name: str = "none", 
+            current_date: str = "January 1, 2020"
+        ) -> None:
         self.customer_name = customer_name
         self.current_date = current_date
         self.cart_items = []
+        print(f"Customer name: {self.customer_name}")
+        print(f"Today's date: {self.current_date}")
 
-    def get_item(self, item_name = "none"):
+    def get_item(self, item_name: str = "none") -> str:
         for item in self.cart_items:
             if item.item_name == item_name:
                 return item
         return f"Item {item_name} does not exist in cart."
 
-    def add_item(self, itemtoPurchase: ItemtoPurchase = None):
+    def add_item(self, 
+            itemtoPurchase: ItemtoPurchase = None
+        ) -> None:
         self.cart_items.append(itemtoPurchase)
 
-    def remove_item(self, item_name: str = ""):
+    def remove_item(self, item_name: str = "") -> None:
         for item in self.cart_items:
             if item.item_name == item_name:
+                
                 self.cart_items.remove(item)
                 return 
         print("Item not found in cart. Nothing removed.")
 
-    def modify_item(self, itemtoPurchase: ItemtoPurchase = None):
-        modify_choicer = 'q' #input("Modify item price, quantity, or description? (p/q/d): ")
-        if modify_choicer == 'p':
+    class ModifyChoice(str, Enum):
+        PRICE = 'p'
+        QUANTITY = 'q'
+        DESCRIPTION = 'd'
+
+    def modify_item(self, 
+            itemtoPurchase: ItemtoPurchase = None,
+            modify_choice:  ModifyChoice = ModifyChoice.PRICE
+        ) -> None:
+        if modify_choice == self.ModifyChoice.PRICE:
             itemtoPurchase.item_price = int(input("Enter the new price: "))
-        elif modify_choicer == 'q':
+        elif modify_choice == self.ModifyChoice.QUANTITY:
             itemtoPurchase.item_quantity = int(input("Enter the new quantity: "))
-        elif modify_choicer == 'd':
+        elif modify_choice == self.ModifyChoice.DESCRIPTION:
             itemtoPurchase.description = input("Enter the new description: ")
         else:
             print("Invalid option")
@@ -46,7 +70,7 @@ class ShoppingCart():
     def get_num_items_in_cart(self) -> int:
         return len(self.cart_items)
     
-    def print_total(self):
+    def print_total(self) -> None:
         if self.get_num_items_in_cart() == 0:
             print("SHOPPING CART IS EMPTY")
         else:
@@ -65,7 +89,7 @@ class ShoppingCart():
             total_cost += item.item_price * item.item_quantity
         return total_cost
 
-    def print_cart(self):
+    def print_cart(self) -> None:
         print("OUTPUT SHOPPING CART")
         print(f"{self.customer_name}'s Shopping Cart - {self.current_date}")
         print(f"Number of Items: {self.get_num_items_in_cart()}")
@@ -76,7 +100,7 @@ class ShoppingCart():
         print()
         print(f"Total: ${total_cost:.2f}")
 
-    def print_description(self):
+    def print_description(self) -> None:
         print("OUTPUT ITEMS' DESCRIPTIONS")
         print(f"{self.customer_name}'s Shopping Cart - {self.current_date}")
         print()
